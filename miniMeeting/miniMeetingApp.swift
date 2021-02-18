662291727b5466da8c327f650f360aeb7cc7a9f0
miguelferreira <mferreira.oeiras@gmail.com>
2021-02-18 00:31:12 +0000

    Initial Commit

---
+++ miniMeeting/miniMeetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniMeetingApp.swift
+//  miniMeeting
+//
+//  Created by Miguel Ferreira on 18/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct miniMeetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
